b9443828e0a447728baf9a300d84693df440741e
khaled07-ag <khaled-kk01@hotmail.com>
2024-09-18 21:08:37 +0300

    3/4

--- api/accounts/accounts.controllers.js
+++ api/accounts/accounts.controllers.js
@@ -1,6 +1,7 @@
 let accounts = require('../../accounts');
 const accountsSchema = require('../../models/accountsSchema');
 
+
 exports.accountCreate = async (req, res) => {
   // const id = accounts[accounts.length - 1].id + 1;
   // const newAccount = { ...req.body, funds: 0, id };
@@ -9,42 +10,61 @@ exports.accountCreate = async (req, res) => {
   try {
     const accountInfo = req.body;
     const newAccount = await accountsSchema.create(accountInfo)
-    return req.status(201).json({data: newAccount});
+    return res.status(201).json({data: newAccount});
   } catch (error) {
     console.log(error);
-    return req.status(500).json({error: error})
+    return res.status(500).json({error: error})
   }
 };
 
-exports.accountDelete = (req, res) => {
-  const { accountId } = req.params;
-  const foundAccount = accounts.find((account) => account.id === +accountId);
-  if (foundAccount) {
-    accounts = accounts.filter((account) => account.id !== +accountId);
-    res.status(204).end();
-  } else {
-    res.status(404).json({ message: 'Account not found' });
+exports.accountDelete = async (req, res) => {
+  // const { accountId } = req.params;
+  // const foundAccount = accounts.find((account) => account.id === +accountId);
+  // if (foundAccount) {
+  //   accounts = accounts.filter((account) => account.id !== +accountId);
+  //   res.status(204).end();
+  // } else {
+  //   res.status(404).json({ message: 'Account not found' });
+  // }
+  try {
+    const {accountId} = req.params;
+    const deleteAccount = await accountsSchema.findByIdAndDelete(accountId);
+    return res.status(204).json({message: deleteAccount});
+  } catch (error) {
+    console.log(error)
+    return res.status(404).json({error: error})
   }
 };
 
-exports.accountUpdate = (req, res) => {
-  const { accountId } = req.params;
-  const foundAccount = accounts.find((account) => account.id === +accountId);
-  if (foundAccount) {
-    foundAccount.funds = req.body.funds;
-    res.status(204).end();
-  } else {
-    res.status(404).json({ message: 'Account not found' });
+exports.accountUpdate = async (req, res) => {
+  // const { accountId } = req.params;
+  // const foundAccount = accounts.find((account) => account.id === +accountId);
+  // if (foundAccount) {
+  //   foundAccount.funds = req.body.funds;
+  //   res.status(204).end();
+  // } else {
+  //   res.status(404).json({ message: 'Account not found' });
+  // }
+  try {
+    const {accountId} = req.params;
+    const updateAccount = await accountsSchema.findByIdAndUpdate(accountId, req.body)
+    if(!updateAccount){
+      return res.status(404).json({error: "not found"})
+    }
+    return res.status(204).json({message:"account updated", data: updateAccount});
+   
+  } catch (error) {
+    return res.status(500).json({error: error})
   }
 };
 
 exports.accountsGet = async (req, res) => {
   try {
     const accounts = await accountsSchema.find();
-    return req.status(200).json({data: accounts});
+    return res.status(200).json({data: accounts});
   } catch (error) {
     console.log(error);
-    return req.status(500)
+    return res.status(500)
   }
 };
 
--- app.js
+++ app.js
@@ -2,10 +2,16 @@ let accounts = require('./accounts');
 const express = require('express');
 const app = express();
 const accountsRoutes = require('./api/accounts/accounts.routes');
+const connectDB = require("./database");
+const dotenv = require("dotenv");
+const morgan = require('morgan');
+dotenv.config();
+connectDB();
 
 app.use(express.json());
+app.use(morgan("dev"));
 app.use('/accounts', accountsRoutes);
 
-app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+app.listen(process.env.PORT, () => {
+  console.log(`The application is running on localhost:${process.env.PORT}`);
 });
